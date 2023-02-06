429484afe38265c5d1136ca9c0263b04f754e80a
MHM <mohsen.mhm23@gmail.com>
2023-02-06 22:17:37 +0330

    First commit, add Authentication with Tailwind.css

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+    <div
+        class="flex flex-col justify-center items-center w-4/5 p-4 min-h-0 max-h-96 bg-white border border-gray-200 rounded-lg shadow-2xl sm:p-6 md:p-8 dark:bg-gray-800/70 dark:border-gray-700">
+        <h5 class="text-xl mb-2 font-medium text-gray-900 dark:text-white">Login to site</h5>
+
+        <form method="POST" action="{{ route('login') }}" class="w-full">
+            @csrf
+            <div class="relative z-0 w-full mb-6 group">
+                <input type="email" name="email" id="email"
+                       class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                       placeholder=" " value="{{ old('email') }}" required/>
+                <label for="email"
+                       class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email
+                    address</label>
+                @error('email')
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span
+                        class="font-medium">Oops!&nbsp;</span>{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="relative z-0 w-full mb-6 group">
+                <input type="password" name="password" id="password"
+                       class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                       placeholder=" " required/>
+                <label for="password"
+                       class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                @error('password')
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span
+                        class="font-medium">Oops!&nbsp;</span>{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="flex items-start mb-6">
+                <div class="flex items-center h-5">
+                    <input name="remember" id="remember" type="checkbox" value=""
+                           class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" {{ old('remember') ? 'checked' : '' }}>
+                </div>
+                <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
+                    me</label>
+            </div>
+            <button type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Login
+            </button>
+            @if (Route::has('password.request'))
+                <div class="relative z-0 w-full mt-5 group">
+                    <a href="{{ route('password.request') }}"
+                       class="font-medium text-blue-600 hover:underline dark:text-blue-500">
+                        {{ __('Forgot Your Password?') }}
+                    </a>
+                </div>
+            @endif
+        </form>
+    </div>
+@endsection
