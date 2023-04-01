0703bc9f56e8a01c1045f797e4de3ca47e76c80c
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-04-01 15:22:02 +0430

    Working on API - add login and register api

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\v1\LoginRequest;
+use App\Http\Requests\Api\v1\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request): Response
+    {
+        try {
+            $credentials = $request->only('email', 'password');
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+                $token = $user->createToken('authToken')->accessToken;
+
+                return response([
+                    'message' => 'Login successfully',
+                    'data' => [
+                        'user' => $user,
+                        'token' => $token
+                    ]
+                ], Response::HTTP_OK);
+            }
+            return response([
+                'message' => 'Invalid credentials',
+                'data' => []
+            ], Response::HTTP_UNAUTHORIZED);
+        } catch (\Exception $exception) {
+            return response([
+                'message' => $exception->getMessage(),
+                'data' => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function register(RegisterRequest $request): Response
+    {
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            $token = $user->createToken('authToken')->accessToken;
+
+            return response([
+                'message' => 'Register successfully',
+                'data' => [
+                    'user' => $user,
+                    'token' => $token
+                ]
+            ], Response::HTTP_OK);
+
+        } catch (\Exception $exception) {
+            return response([
+                'message' => $exception->getMessage(),
+                'data' => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::prefix('v1')->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+    });
 });
