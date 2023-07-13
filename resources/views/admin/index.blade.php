d65efdbdc2985b1a935e68517428536b1d1cd1c7
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-07-13 21:05:48 +0430

    Minor bugs fixed

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,6 +16,7 @@
             <div class="pl-4 font-bold inline-flex">Users:&nbsp;&nbsp;<p
                     class="text-blue-400">{{ \App\Models\User::count() }}</p></div>
         </a>
+
         <a href="{{ route('admin.exams.index') }}" id="toast-simple" type="button"
            class="cursor-pointer flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow-lg border border-gray-700 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800 m-2"
            role="alert">
