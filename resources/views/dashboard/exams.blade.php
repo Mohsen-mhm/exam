b510274b93c75043ce3f0b982a6f879f34b72070
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-07-14 20:08:00 +0430

    Minor bugs fixed

--- resources/views/dashboard/exams.blade.php
+++ resources/views/dashboard/exams.blade.php
@@ -17,6 +17,7 @@
                         Dashboard
                     </a>
                 </li>
+                
                 <li class="mr-2">
                     <a href="{{ route('profile.exams') }}"
                        class="inline-flex p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group"
